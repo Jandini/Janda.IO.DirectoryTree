7e5d8f464f96a16b06f6e2f716baca7adb835b7d
Matt Janda <matt.janda.kingston@gmail.com>
2022-05-01 19:50:02 +0100

    Add directory tree

---
+++ src/Janda.IO.DirectoryTree/DirectoryTree.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Janda.IO
+{
+    public sealed class DirectoryTree
+    {
+        internal static void Traverse(DirectoryTreeInfo dtInfo, string path, Action<DirectoryTreeInfo> dir, Action<DirectoryTreeInfo> file, Action<Exception> exception)
+        {
+            try
+            {
+                foreach (var treeItem in new DirectoryInfo(path).GetFileSystemInfos())
+                {
+                    switch (treeItem)
+                    {
+                        case DirectoryInfo dirInfo:
+                            var treeDir = new DirectoryTreeInfo(dirInfo, dtInfo, path, 0);
+                            dir?.Invoke(treeDir);
+                            Traverse(treeDir, Path.Combine(path, treeItem.Name), dir, file, exception);
+                            break;
+
+                        case FileInfo fileInfo:
+                            if (file is not null)
+                                file.Invoke(new DirectoryTreeInfo(fileInfo, dtInfo, path, fileInfo.Length));
+                            break;
+
+                        default:
+                            throw new NotSupportedException();
+
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                exception?.Invoke(ex);
+            }
+        }
+
+
+
+        internal static IEnumerable<DirectoryTreeInfo> Traverse(DirectoryTreeInfo dtInfo, string path, Action<Exception> exception)
+        {
+
+            FileSystemInfo[] treeItems = null;
+
+            try
+            {
+                treeItems = new DirectoryInfo(path).GetFileSystemInfos();
+            }
+            catch (Exception ex)
+            {
+                exception?.Invoke(ex);
+            }
+
+            if (treeItems is not null)
+            {
+                foreach (var treeItem in treeItems)
+                {
+                    switch (treeItem)
+                    {
+                        case DirectoryInfo dirInfo:
+                            var treeDir = new DirectoryTreeInfo(dirInfo, dtInfo, path, 0);
+                            yield return treeDir;
+
+                            foreach (var item in Traverse(treeDir, Path.Combine(path, dirInfo.Name), exception))
+                                yield return item;
+
+                            break;
+
+                        case FileInfo fileInfo:
+                            yield return new DirectoryTreeInfo(fileInfo, dtInfo, path, fileInfo.Length);
+                            break;
+
+                        default:
+                            throw new NotSupportedException();
+                    }
+                }
+            }
+        }
+
+
+        private static string AddPathSeparator(string path) => !path.EndsWith(Path.DirectorySeparatorChar) ? path += Path.DirectorySeparatorChar : path;
+        private static DirectoryTreeInfo GetFirstTreeInfo(string path) => new(AddPathSeparator(path));
+        private static DirectoryTreeInfo GetFirstTreeInfo(string path, Guid parent) => new(AddPathSeparator(path), parent);
+
+
+
+        public static void Traverse(string path, Action<DirectoryTreeInfo> dir, Action<DirectoryTreeInfo> file, Action<Exception> exception) => Traverse(GetFirstTreeInfo(path), AddPathSeparator(path), dir, file, exception);
+        public static void Traverse(string path, Guid parent, Action<DirectoryTreeInfo> dir, Action<DirectoryTreeInfo> file, Action<Exception> exception) => Traverse(GetFirstTreeInfo(path, parent), AddPathSeparator(path), dir, file, exception);
+
+
+        public static IEnumerable<DirectoryTreeInfo> Traverse(string path, Action<Exception> exception) => Traverse(GetFirstTreeInfo(path), AddPathSeparator(path), exception);
+        public static IEnumerable<DirectoryTreeInfo> Traverse(string path, Guid parent, Action<Exception> exception) => Traverse(GetFirstTreeInfo(path, parent), AddPathSeparator(path), exception);
+        public static IEnumerable<DirectoryTreeInfo> Traverse(string path) => Traverse(GetFirstTreeInfo(path), AddPathSeparator(path), null);
+        public static IEnumerable<DirectoryTreeInfo> Traverse(string path, Guid parent) => Traverse(GetFirstTreeInfo(path, parent), AddPathSeparator(path), null);
+    }
+}
---
+++ src/Janda.IO.DirectoryTree/DirectoryTreeInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Janda.IO
+{
+    public sealed class DirectoryTreeInfo
+    {
+        private readonly int _rootLength = 0;
+
+        public Guid Id { get; set; }
+        public Guid Parent { get; set; }
+        public int DirectoryLevel { get; set; }
+        public string RootName { get; set; }
+        public string DirectoryName { get; set; }
+        public string Name { get; set; }
+        public string FullName => Path.Combine(DirectoryName, Name);
+        public string RelativeName => Path.Combine(DirectoryName[_rootLength..], Name);
+        public FileAttributes Attributes { get; set; }
+        public long Size { get; set; }
+        public DateTime CreationTimeUtc { get; set; }
+        public DateTime LastAccessTimeUtc { get; set; }
+        public DateTime LastWriteTimeUtc { get; set; }
+
+
+        public DirectoryTreeInfo(string path)
+        {
+            Id = Guid.Empty;
+            DirectoryLevel = -1;
+            _rootLength = path.Length;
+        }
+
+        public DirectoryTreeInfo(string path, Guid id)
+        {
+            Id = id;
+            DirectoryLevel = -1;
+            _rootLength = path.Length;
+        }
+
+        public DirectoryTreeInfo(FileSystemInfo fsInfo, DirectoryTreeInfo dtInfo, string path, long size)
+        {
+            _rootLength = dtInfo._rootLength;
+
+            Id = Guid.NewGuid();
+            Parent = dtInfo.Id;
+            DirectoryName = path;
+            DirectoryLevel = dtInfo.DirectoryLevel + 1;
+            Name = fsInfo.Name;
+            Size = size;
+            Attributes = fsInfo.Attributes;
+            LastAccessTimeUtc = fsInfo.LastAccessTimeUtc;
+            CreationTimeUtc = fsInfo.CreationTimeUtc;
+            LastWriteTimeUtc = fsInfo.LastWriteTimeUtc;
+        }
+    }
+}
